b9b83ac51f7a4e57e6308218e4907c9e66e2b92a
Peter Tran <megapolis661@gmail.com>
2026-06-10 01:01:26 +0700

    remove debug log

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,6 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-console.log("[db] DATABASE_URL host:", process.env.DATABASE_URL?.match(/@([^:/]+)/)?.[1]);
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 export const db = globalThis.prisma || new PrismaClient({ adapter });
